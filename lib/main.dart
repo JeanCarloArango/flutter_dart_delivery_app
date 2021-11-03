04157c8cd92607c0560316ddbe6321b6901160e0
Jean Carlo Arango <jeankrlo.2000@hotmail.com>
2021-11-03 10:26:20 -0500

    Starting Home Page

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:delivery_app/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MainApp(),
+  );
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Delivery App',
+      theme: ThemeData.light(),
+      home: HomePage(),
+    );
+  }
+}
